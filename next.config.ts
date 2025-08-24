2f868eae34d033d2acf8da70989b8ceded56df3a
isis <issei.soccer618@gmail.com>
2025-08-24 14:18:58 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'corporate-site';
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   output: "export",
   images: {
     unoptimized: true,
